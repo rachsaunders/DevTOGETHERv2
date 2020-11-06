f4bbb809e459f451f1c186a004fc28c91324225b
Rachel Saunders <rachelsaundersBA@gmail.com>
2020-11-06 14:55:27 +0000

    Initial Commit

---
+++ DevTOGETHERv2Tests/DevTOGETHERv2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  DevTOGETHERv2Tests.swift
+//  DevTOGETHERv2Tests
+//
+//  Created by Rachel Saunders on 06/11/2020.
+//
+
+import XCTest
+@testable import DevTOGETHERv2
+
+class DevTOGETHERv2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
